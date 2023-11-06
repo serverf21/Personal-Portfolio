6c0f28c5cf4591b862310e6160128d212ef70ac1
Sarvagya <30923855+serverf21@users.noreply.github.com>
2023-11-06 11:42:45 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -95,7 +95,7 @@ const Banner = () => {
                   </button>
                 </Link>
               </div>
-              <a href="https://drive.google.com/file/d/1lYM71UiSuLAXj0cFr6pUPS_MpQCOD0UQ/view?usp=sharing"
+              <a href="https://drive.google.com/file/d/1YfMUvZTTB4NXQJtojXUbBsO8s0vuhLUj/view?usp=sharing"
                 target="_blank" rel="noreferrer">
                 <div
                   className="px-4 py-3 border-b-4 border-cyan-200 text-cyan-200 hover:text-white hover:bg-cyan-700
